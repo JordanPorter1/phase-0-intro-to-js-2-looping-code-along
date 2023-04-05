6ad1015aaad2e4855aa6b8937f2b36e7cbf88425
Jordan Porter <s.jordanporter@hotmail.com>
2023-04-05 08:40:39 -0600

    final

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+let names = ['Guadalupe', "Ollie", "Aki"]
+
+function writeCards(names) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+    cards[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+    
+    }
+      
+    return cards
+
+}
+
+writeCards(names);
+
+function countDown(num) {
+    let i = num
+    for(i = num; i >= 0; i--) {
+        console.log(i)
+    }
+}
